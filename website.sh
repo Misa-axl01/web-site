37d0fb0ebb51810b2b1df3d7c14d56c6c18adccf
Misa-axl01 <56968615+Misa-axl01@users.noreply.github.com>
2023-08-22 12:17:22 -0600

    Update website.sh

--- website.sh
+++ website.sh
@@ -2,3 +2,4 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 echo "<h1>Hola mundo</h1><p>Hostname: $(hostname)</p>" | sudo tee /var/www/html/index.html
+sudo systemctl restart httpd
